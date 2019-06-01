664c6793cb04a4c6df25482900b10784af8350b1
Daniel Prince <danielahprince@live.com>
2019-06-01 17:38:50 +0200

    fixed bug preventing user creation
    Didn't allow author creation when there no authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,7 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :edit, :update, :destroy]
-  before_action :zero_authors_or_authenticated, only: [:new, :create]
-  before_action :require_login, except: [:new, :create]
+  before_action :zero_authors_or_authenticated, except: [:index]
+
 
   def zero_authors_or_authenticated
     unless Author.count == 0 || current_user
